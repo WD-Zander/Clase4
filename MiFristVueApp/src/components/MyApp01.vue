532f7bb51ba03d3684d5f35f5a8428ea9e0f8ef2
Zander <zanderjosue05.com>
2024-12-14 15:24:29 -0400

    frist commit

---
+++ MiFristVueApp/src/components/MyApp01.vue
@@ -0,0 +1,16 @@
+<script></script>
+<template>
+    <div>
+        Mi Primera APP en  <span>Vue.Js</span>
+    </div>
+</template>
+<style scoped>
+    div {
+            color: #35495f;
+            font-size: 1.6rem;
+    }
+
+    span {
+        color: #41b883;
+    }
+</style>
